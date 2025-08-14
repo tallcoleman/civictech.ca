786d0d1c8a0695b20963d46b3c5867b2d3151422
Iain Campbell <hey@iainmc.ca>
2025-08-13 21:53:20 -0400

    clean up script

--- _scripts/thumbnail_sizes.sh
+++ _scripts/thumbnail_sizes.sh
@@ -1,19 +1,23 @@
+# In order to avoid layout shift when lazy loading the images
+# on the hacknights page, we need to make their dimensions
+# available to the server so we can render correct placeholder.
+#
+# This script runs as part of the build process.
+
 thumb_dir="assets/images/hacknights/thumbnails"
 thumbs=($thumb_dir/*.jpg)
 
-touch "$thumb_dir/thumbs.json"
-truncate -s 0 "$thumb_dir/thumbs.json"
-echo "{" >> "$thumb_dir/thumbs.json"
+file=$thumb_dir/thumbs.json
+
+# wipe the contents
+touch $file
+truncate -s 0 $file
+echo "{" >> $file
 
 for thumb in "${thumbs[@]}" ; do
   filename=$(basename "$thumb")
-
-  echo $filename
-
-  # Record dimensions
   dimensions=$(magick identify -format "%w,%h\n" "$thumb")
-
-  echo "  \"$filename\": \"$dimensions\"," >> "$thumb_dir/thumbs.json"
+  echo "  \"$filename\": \"$dimensions\"," >> $file
 done
 
-echo "  \"coda\": \"\"\n}" >> "$thumb_dir/thumbs.json"
+echo "  \"coda\": \"\"\n}" >> $file
